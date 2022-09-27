2ac4447a65877d02fb6c8a4eadb1650abbb067c6
gracel0721 <35743830+gracel0721@users.noreply.github.com>
2022-09-27 00:19:02 -0400

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/flixter/Movie.kt
+++ app/src/main/java/com/example/flixter/Movie.kt
@@ -1,14 +1,23 @@
 package com.example.flixter
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 data class Movie (
     val movieId: Int,
+    val voteAvg: Double,
     val title: String,
     val overview: String,
     private val posterPath: String
-){
+) : Parcelable {
+
     val posterImageUrl ="https://image.tmdb.org/t/p/w342/$posterPath"
+
+
+
+
     companion object{
         fun fromJSONarr(movieArr: JSONArray) :List<Movie> {
             val movies = mutableListOf<Movie>()
@@ -17,6 +26,7 @@ data class Movie (
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("title"),
                         movieJson.getString("overview"),
                         movieJson.getString("poster_path")
@@ -28,4 +38,5 @@ data class Movie (
         }
 
     }
+
 }
--- app/src/main/java/com/example/flixter/MovieAdapter.kt
+++ app/src/main/java/com/example/flixter/MovieAdapter.kt
@@ -1,36 +1,50 @@
 package com.example.flixter
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: MutableList<Movie>):
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener{
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
         private val pic = itemView.findViewById<ImageView>(R.id.titlePic)
+        init{
+            itemView.setOnClickListener(this)
+        }
         fun bind(movie: Movie){
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             Glide.with(context).load(movie.posterImageUrl).into(pic)
         }
+         override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+            Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+            val intent= Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        Log.i(TAG, "onCreateViewHolder")
+        Log.i(TAG, "OnCreateViewHolder")
         val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Log.i(TAG, "onBindViewHolder: position $position")
+        Log.i(TAG, "OnBindViewHolder: position $position")
         val movie = movies[position]
         holder.bind(movie)
 
